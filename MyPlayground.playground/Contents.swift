e0a88708c5b0263be1584bbea6734de0f2490a56
Ivan Kos <kosenkov@Ivans-iMac.local>
2023-06-22 17:27:55 +0300

    Task 2, 3 done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,14 +1,12 @@
 //1. Создать структуру работника пиццерии. В ней должны быть такие свойства, как имя, зарплата и должность. Должностей пока может быть две: или кассир, или повар. Добавить в класс пиццерии массив с работниками.
 class Cafe{
-//    fileprivate var menu: [MenuProtocol]
     var worker: [Worker]
-    init (worker: [Worker]){
+    var tables: [Table]
+    init (worker: [Worker], tables: [Table]){
         self.worker = worker
+        self.tables = tables
     }
-    
-//    init (menu: [MenuProtocol]){
-//        self.menu = menu
-//    }
+
     func add(_ new: Worker){
         worker.append(new)
     }
@@ -36,6 +34,28 @@ cafe.add(worker3)
 print(cafe.get())
 
 //2. Создать класс столика, в нём должно быть свойство, в котором содержится количество мест и функция, которая на вход принимает количество гостей, которое хотят посадить, а возвращает true, если места хватает и false, если места не хватает. Изначальное количество мест задаётся в инициализаторе.
-
-
 //3. Добавить в класс пиццерии свойство, в котором хранится массив столиков. У класса столика добавить свойство, в котором хранится кафе, в котором стоит столик. Столики создаются сразу в инициализаторе, не передаются туда в качестве параметра.
+
+class Table{
+    var places: [Int]
+    var cafe: Cafe
+//    var cafe: Cafe
+    init (places: [Int], cafe: Cafe){
+        self.places = [4, 3, 5]
+        self.cafe = cafe
+    }
+    
+    func freePlaces(guests: Int){
+        for item in places{
+            if guests <= item{
+                print(true)
+            }else{
+                print(false)
+            }
+        }
+    }
+}
+
+var table = Table(places: [])
+
+print(table.freePlaces(guests: 4))
